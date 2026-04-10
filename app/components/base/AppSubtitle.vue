bb196566d4af1514caac2e58e464b3cc0cf9d29a
FizzleOut <fizzleout1337@gmail.com>
2026-04-10 16:18:36 +0200

    feat(AppSubtitle): add subtitle component
    - Implement AppSubtitle using h2 element
    - Support slot for subtitle content
    - Add styles using design tokens and Orbitron font
    - Implement responsive font sizes

---
+++ app/components/base/AppSubtitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="subtitle">
+    <slot/>
+  </h2>
+</template>
+
+<style scoped>
+  .subtitle {
+    font-weight: var(--fw-semibold);
+    font-size: var(--fs-2xl);
+    color: var(---color-text-primary);
+    letter-spacing: 0.05em;
+    font-family: "Orbitron", sans-serif;
+  }  
+  @media (max-width: 768px) {
+    .subtitle {
+      font-size: var(--fs-xl);
+    }
+  }
+  @media (max-width: 499.98px) {
+    .subtitle {
+      font-size: var(--fs-lg);
+    }  
+  }
+</style>
