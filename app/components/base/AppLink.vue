c97858b9048bc45765ce093d88767a7fee172ad3
FizzleOut <fizzleout1337@gmail.com>
2026-04-10 16:19:11 +0200

    feat(AppLink): add link component with hover effect
    - Implement AppLink using slot
    - Add base text styles for link
    - Use line class for underline animation
    - Implement hover effect with gradient text

---
+++ app/components/base/AppLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="link line">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+  .link {
+    font-family: "Montserrat", sans-serif;
+    font-size: var(--fs-md);
+    font-weight: var(--fw-medium);
+    color: var(--color-text-primary);
+    transition: var(--transition-hover);
+    cursor: pointer;
+  }
+
+  .link:hover {
+    background: var(--color-gradient-hover);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
